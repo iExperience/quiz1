d200f15ab38465e34a96c9e470daebf33680accd
eliagchen1 <50680479+eliagchen1@users.noreply.github.com>
2019-05-30 23:55:22 +0100

    Create question4a.js

---
+++ question4a.js
@@ -0,0 +1,19 @@
+
+// Question 4 (1) Code: initialize Person's instance variables in the constructor.
+
+class Person {
+  public name: string;
+  home: string;
+  private phone: int;
+
+  constructor(
+    givenName: string,
+    birthPlace: string,
+    homeNumber: int,
+  ) {
+    ...
+    (a) this.____ = ____;
+    (b) this.____ = ____;
+    (c) this.____ = ____;
+  }
+}
