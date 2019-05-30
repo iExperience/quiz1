26bd3f4230c6730f38fda45bf126ff1c086e537e
eliagchen1 <50680479+eliagchen1@users.noreply.github.com>
2019-05-30 23:54:33 +0100

    Create question2.js

---
+++ question2.js
@@ -0,0 +1,10 @@
+
+// Question 2 Code: What happens when this code is executed?
+let myId = 3;
+if (myId === '3') {
+  myId++;
+  console.log(myId);
+} else if (myId == 3) {
+  myId--;
+  console.log(myId);
+}
